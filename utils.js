f54406cd5d676d3347fd74997acefe1f0eb3eae8
oscarvz <oscar@fiberplane.com>
2023-07-30 14:23:59 +0200

    Add Parcel bundler, update project structure

--- utils.js
+++
@@ -1,6 +0,0 @@
-export function isRgbaValue(value) {
-    return (Object.keys(value).includes("r") &&
-        Object.keys(value).includes("g") &&
-        Object.keys(value).includes("b") &&
-        Object.keys(value).includes("a"));
-}
