44c611ce7e2551a2f634aa8e421e8bb4844dceee
oscarvz <oscarvz@users.noreply.github.com>
2023-08-24 17:33:57 +0200

    Add first setup UI

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { generateFiles } from './plugin';
 
 const { cssFile, jsFile } = generateFiles();
 
-figma.showUI(__html__);
+figma.showUI(__html__, { themeColors: true });
 
 figma.ui.postMessage({
   css: {
