15e6c38f409190eb96ba6d08a611994424424ff3
oscarvz <oscarvz@users.noreply.github.com>
2023-08-24 16:35:39 +0200

    Dedupe variables

--- src/generateVariables.ts
+++ src/generateVariables.ts
@@ -10,9 +10,9 @@ import {
 export function generateVariables(themeObject: object) {
   const variableCollections = figma.variables.getLocalVariableCollections();
 
-  const atomicCssVariables: Array<string> = [];
-  const semanticCssVariablesDefault: Array<string> = [];
-  const semanticCssVariablesDark: Array<string> = [];
+  const atomicCssVariables = new Set<string>();
+  const semanticCssVariablesDefault = new Set<string>();
+  const semanticCssVariablesDark = new Set<string>();
 
   for (const variableCollection of variableCollections) {
     const { modes: collectionModes, variableIds } = variableCollection;
@@ -50,18 +50,18 @@ export function generateVariables(themeObject: object) {
           );
 
           if (collectionMode.name.toLowerCase().includes('dark')) {
-            semanticCssVariablesDark.push(aliasedVariable);
+            semanticCssVariablesDark.add(aliasedVariable);
             continue;
           }
 
-          semanticCssVariablesDefault.push(aliasedVariable);
+          semanticCssVariablesDefault.add(aliasedVariable);
           continue;
         }
 
         switch (figmaVariable.resolvedType) {
           case 'BOOLEAN' || 'STRING': {
             const cssVariable = `${cssVariableName}: ${figmaVariableModeValue};`;
-            atomicCssVariables.push(cssVariable);
+            atomicCssVariables.add(cssVariable);
             break;
           }
           case 'COLOR': {
@@ -71,13 +71,13 @@ export function generateVariables(themeObject: object) {
             if (isValidRgba || isValidRgb) {
               const colorValue = getColorValue(figmaVariableModeValue);
               const cssVariable = `${cssVariableName}: ${colorValue};`;
-              atomicCssVariables.push(cssVariable);
+              atomicCssVariables.add(cssVariable);
             }
             break;
           }
           case 'FLOAT': {
             const cssVariable = `${cssVariableName}: ${figmaVariableModeValue}px;`;
-            atomicCssVariables.push(cssVariable);
+            atomicCssVariables.add(cssVariable);
           }
         }
       }
@@ -85,8 +85,8 @@ export function generateVariables(themeObject: object) {
   }
 
   return {
-    atomicCssVariables,
-    semanticCssVariablesDefault,
-    semanticCssVariablesDark,
+    atomicCssVariables: [...atomicCssVariables],
+    semanticCssVariablesDefault: [...semanticCssVariablesDefault],
+    semanticCssVariablesDark: [...semanticCssVariablesDark],
   };
 }
